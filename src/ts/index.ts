227d1e59d16c68eabd266bb99780dbe93a659b2f
Daniel-De-Dev <github@daniels-sliks.com>
2024-05-11 19:55:37 +0200

    added the click functionality for a piece

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -3,6 +3,9 @@ document.addEventListener('DOMContentLoaded', function() {
     const board = document.getElementById('chessboard');
 
     if (board) {
+
+        board.dataset['highlightedCell'] = "";
+
         // Generate each cell of the board
         for (let row = 0; row < 8; row++) {
             for (let col = 0; col < 8; col++) {
@@ -21,6 +24,7 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
+    // Add a test piece to the board //! TEMP
     let test = document.getElementById('4-4');
 
     if (test) {
@@ -28,6 +32,98 @@ document.addEventListener('DOMContentLoaded', function() {
         img.classList.add('piece');
         img.src = './assets/king-w.svg';
         img.alt = 'white king';
+        img.dataset['positionX'] = '4';
+        img.dataset['positionY'] = '4';
+        img.dataset['piece'] = 'king';
+        img.dataset['color'] = 'white';
         test.appendChild(img);
     }
-})
+
+
+    // Add Click event to all pieces on board
+    const pieces = document.querySelectorAll('.piece');
+    pieces.forEach(piece => {
+        piece.addEventListener('click', handlePieceClick);
+    });
+
+    // Add click event to all cells
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach(cell => {
+        cell.addEventListener('click', handleCellClick);
+    });
+});
+
+function handlePieceClick(event: Event) {
+    const clickedPiece = event.target as HTMLElement;
+    
+    // Get the position for the clicked piece
+    const posX = parseInt(clickedPiece.dataset['positionX'] ?? "-1", 10);
+    const posY = parseInt(clickedPiece.dataset['positionY'] ?? "-1", 10);
+    if (posX === -1 || posY === -1) {
+        console.error(`Getting the clicked piece's coordinates failed (x,y: ${posX},${posY})`, clickedPiece);
+        return;
+    }
+    
+    const board = document.getElementById('chessboard');
+    if (!board) {
+        console.error('couldn\'t get the Chessboard Element');
+        return;
+    }
+    
+    // Get the what the current highlighted cell on the board is
+    const highlighted_cell = board.dataset['highlightedCell'];
+    if (highlighted_cell === undefined) {
+        console.error('Failed to retrieve data of highlighted cell from chessboard', board);
+        return;
+    } 
+
+    if (highlighted_cell.length === 0) {
+        // There are no actively highlighted cells on the board
+        event.stopPropagation(); // Prevent other events from getting triggered
+        clickedPiece.classList.add('selected');
+        board.dataset['highlightedCell'] = `${posX}-${posY}`;
+        
+
+
+        return;
+    }
+}
+
+
+
+function handleCellClick() {
+    const board = document.getElementById('chessboard');
+    if (!board) {
+        console.error('couldn\'t get the Chessboard Element');
+        return;
+    }
+
+    const highlighted_cell = board.dataset['highlightedCell'];
+    if (highlighted_cell === undefined) {
+        console.error('Failed to retrieve data of highlighted cell from chessboard', board);
+        return;
+    }
+
+    const cell = document.getElementById(highlighted_cell);
+    if (cell) {
+        const cellPiece = cell.querySelector('.piece');
+        if (cellPiece) {
+            cellPiece.classList.remove('selected');        
+            board.dataset['highlightedCell'] = '';
+            
+            const dots = document.querySelectorAll('.dot');
+
+            dots.forEach(dot => {
+            dot.remove();    
+            });
+        }
+    }
+}
+    
+    
+
+function handleDotClick(event: Event) {
+    event.stopPropagation(); 
+    //! Remember to update board info
+    console.log('Clicked Dot');
+}
