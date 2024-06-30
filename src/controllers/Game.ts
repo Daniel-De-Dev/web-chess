e3c30ca594b06ffdc724e262a3162a758fbbfbdf
Daniel-De-Dev <github@daniels-sliks.com>
2024-06-30 19:02:53 +0200

    Modified Game class

--- src/controllers/Game.ts
+++ src/controllers/Game.ts
@@ -1,11 +1,19 @@
-import { Color, ChessBoard, Row } from '../interfaces/Types.js';
+import { Color, ChessBoard, Row, ChessPieces, Coordinate } from '../interfaces/Types.js';
 
+/**
+ * Represents a game of chess.
+ */
 export class Game {
     board: ChessBoard;
+    html_board: HTMLElement;
     turn: Color = 1;
     board_direction: Color;
-    constructor(board_direction: Color, initialBoard: ChessBoard) {  
+    highlighted_piece: ChessPieces | null = null;
+    valid_moves: Coordinate[] | null = null;
+
+    constructor(board_direction: Color, initialBoard: ChessBoard, html_board: HTMLElement) {  
         this.board_direction = board_direction;
+        this.html_board = html_board;
         this.board = this.deep_copy_board(initialBoard);
     }
 
