8d6433ea3499aea011e471f13a3d98fd7ecdfa83
Daniel-De-Dev <github@daniels-sliks.com>
2024-08-09 03:02:11 +0200

    Added movement functionality (no checks)

--- src/services/Game.ts
+++ src/services/Game.ts
@@ -1,5 +1,7 @@
 import { Game } from "../controllers/Game.js";
+import { Color } from "../interfaces/Types.js";
 import { BOARD_SIZE } from "../models/Board.js";
+import { Pawn, Rook, King } from "../models/Piece.js";
 import { draw_board } from "../views/BoardView.js";
 import { get_valid_moves } from "./Validation.js";
 
@@ -15,8 +17,9 @@ export function handle_square_click(event: Event, game: Game) {
             piece_click(CLICKED_TARGET, CELL, game, 0);
 
         } else if (TARGET_CLASSES.contains('dot')) { // a dot has been pressed
+            
+            dot_click(CELL, game, 0);
 
-            // TODO: Complete dot presses (piece is being moved)
         }
 
     }
@@ -90,4 +93,76 @@ function piece_click(_piece_element: HTMLElement, cell_element: HTMLElement, gam
     draw_board(game.html_board, game);
     
     return;
+}
+
+function dot_click(cell_element: HTMLElement, game: Game, attempt: number) {
+
+    if (attempt >= 10) {
+        console.error('After refreshing 10 times, still cannot fix problem', cell_element, game);
+        return;
+    }
+
+    const PARTS = cell_element.id.split('-');
+
+    if (PARTS.length !== 2) {
+        console.error('did not get expected ID, refreshing and retrying', cell_element);
+        draw_board(game.html_board, game);
+        setTimeout(() => dot_click(cell_element, game, attempt + 1), 1000);
+        return;
+    }
+
+    const COLUMN = parseInt(PARTS[0] ?? '-1', 10);
+    const ROW = parseInt(PARTS[1] ?? '-1', 10);
+
+    if (!((0 <= COLUMN && COLUMN < BOARD_SIZE) && (0 <= ROW && ROW < BOARD_SIZE))) {
+        console.error('did not get valid position values, refreshing and retrying', cell_element);
+        draw_board(game.html_board, game);
+        setTimeout(() => dot_click(cell_element, game, attempt + 1), 1000);  
+        return;
+    }
+
+    //! feeling lazy so i wont be doing any validation
+
+    if (!game.highlighted_piece) {
+        console.error('???? how you clicking dots without a selected piece?', game);
+        return;
+    }
+
+    const SELECTED_PIECE_COPY = game.highlighted_piece.clone();
+    SELECTED_PIECE_COPY.position = {column: COLUMN, row: ROW};
+
+    const OLD_ROW = game.highlighted_piece.position.row;
+    const OLD_COLUMN = game.highlighted_piece.position.column;
+
+    if ((SELECTED_PIECE_COPY instanceof Pawn || SELECTED_PIECE_COPY instanceof Rook || SELECTED_PIECE_COPY instanceof King)) {
+        SELECTED_PIECE_COPY.moved = true;
+    }
+
+    const PRE_ROW = game.board[OLD_ROW];
+
+    if (!PRE_ROW) {
+        console.error('The old position of the selected piece doesnt result in a valid row', game);
+        return;
+    }
+
+    if (PRE_ROW[OLD_COLUMN]) {
+        PRE_ROW[OLD_COLUMN] = null;
+    }
+
+    const GAME_ROW = game.board[ROW];
+
+    if (!GAME_ROW) {
+        console.error('The new position of the selected piece doenst result in a valid row', game);
+        return;
+    }
+
+    GAME_ROW[COLUMN] = SELECTED_PIECE_COPY;
+
+    game.valid_moves = null;
+    game.highlighted_piece = null;
+    game.turn *= -1;
+    game.board_direction = game.turn as Color;
+
+    draw_board(game.html_board, game);
+
 }
